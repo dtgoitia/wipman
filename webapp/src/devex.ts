8039a59eaea4aae5803996b312dc66e7c0be4821
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-04-22 20:27:45 +0100

    devex(ui): improve `unreachable` function

--- webapp/src/devex.ts
+++ webapp/src/devex.ts
@@ -2,6 +2,8 @@ export function todo({ message }: { message?: string }): never {
   throw new Error(message ? message : "TODO");
 }
 
-export function unexpected(message: string): never {
-  throw new Error(`UNEXPECTED CODE PATH REACHED: ${message}`);
+export function unreachable(
+  args: { message: string } | undefined = undefined
+): Error {
+  return new Error(args && args.message ? args.message : "TODO");
 }
