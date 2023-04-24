6eb6eabf0620a2e02a13cf4468b8e5be6e507502
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-04-24 19:04:15 +0100

    feat(api): do not start API without DB file

--- api/src/api.py
+++ api/src/api.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 
 from flask import Flask, request
 from flask_cors import CORS
@@ -8,6 +9,8 @@ from src.use_cases.read_from_db import read_tasks_updated_after, read_view_updat
 from src.use_cases.update_items import create_task as create_task_in_db
 from src.use_cases.update_items import update_task as update_task_in_db
 from src.use_cases.update_items import update_view as update_view_in_db
+
+logger = logging.getLogger(__name__)
 
 config = get_config()
 
@@ -80,6 +83,16 @@ def update_view():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    logger.info(f"config: {config}")
+
+    if not config.db_path.exists():
+        exit(
+            f"Expected to find DB file at {config.db_path.absolute()} but"
+            " it does not exist. Aborting..."
+        )
+
     # By default, flask serves in `localhost`, which makes the webserver
     # inaccessible once you containerize it.
     host='0.0.0.0'
