6e4600e07ad87d21e3ab7aab6c238527c5dd0395
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-04-27 08:39:19 +0100

    test(api): update e2e test to use /health

--- api/tests/e2e.py
+++ api/tests/e2e.py
@@ -1,10 +1,13 @@
 import requests
 
-host="128.140.50.206"
-port=5000
-base_url = f'http://{host}:{port}'
+host = "128.140.50.206"
+# host = "localhost"
+port = 5000
+base_url = f"http://{host}:{port}"
 
-url=f'{base_url}/get-all'
+url = f"{base_url}/health"
 print(f"GET {url}")
 response = requests.get(url=url)
+
 print(response)
+print(response.json())
