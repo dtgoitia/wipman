9eb5801431e35aff980be8b28d2700ec7c3a16bd
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-05-03 07:19:32 +0100

    feat(api): improve e2e test

--- api/tests/e2e.py
+++ api/tests/e2e.py
@@ -1,13 +1,44 @@
 import requests
 
-host = "128.140.50.206"
-# host = "localhost"
-port = 5000
-base_url = f"http://{host}:{port}"
+USE_LOCAL = False
+USE_LOCAL = True
+if USE_LOCAL:
+    api_url = "http://localhost:5000"
+else:
+    api_url = "https://api.dtgoitia.dev"
 
-url = f"{base_url}/health"
-print(f"GET {url}")
-response = requests.get(url=url)
 
-print(response)
-print(response.json())
+def check_health() -> None:
+    url = f"{api_url}/health"
+    print(f"GET {url}")
+    response = requests.get(url=url)
+
+    response.raise_for_status()
+    print(response.json())
+
+
+check_health()
+
+
+# url = f"{api_url}/task"
+# print(f"POST {url}")
+# response = requests.post(
+#     url=url,
+#     json={
+#         "task": {
+#             "id": "9740a3fd52",
+#             "title": "adasdasdddas",
+#             "content": "",
+#             "created": "2023-05-02T05:27:38.397Z",
+#             "updated": "2023-05-02T05:27:38.397Z",
+#             "tags": "",
+#             "blocked_by": "",
+#             "blocks": "",
+#             "completed": False,
+#         }
+#     },
+# )
+
+
+# print(response)
+# print(response.json())
