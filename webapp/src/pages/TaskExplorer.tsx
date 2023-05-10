c6070938300323237ac850bdd608edbbf6d6141b
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-05-10 18:58:54 +0100

    chore(ui): remove unused code

--- webapp/src/pages/TaskExplorer.tsx
+++ webapp/src/pages/TaskExplorer.tsx
@@ -3,8 +3,7 @@ import CenteredPage from "../components/CenteredPage";
 import ListedTask from "../components/ListedTask";
 import SearchBox, { NO_FILTER_QUERY } from "../components/SearchBox";
 import { FilterQuery, Task, TaskId, TaskTitle } from "../domain/types";
-import { Wipman, WipmanStatus } from "../domain/wipman";
-import { assertNever } from "../exhaustive-match";
+import { Wipman } from "../domain/wipman";
 import { getTaskPath } from "../routes";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -13,18 +12,12 @@ interface TaskExplorerProps {
   wipman: Wipman;
 }
 function TaskExplorer({ wipman }: TaskExplorerProps) {
-  // TODO: action: open task view
-  const navigate = useNavigate();
+  const navigateTo = useNavigate();
 
   const [query, setQuery] = useState<FilterQuery>(NO_FILTER_QUERY);
   const [tasks, setTasks] = useState<Task[]>([]);
-  // TODO: do we even care about showing the spinner? -- maybe when we are updating one task?
-  const [showSpinner, setShowSpinner] = useState(true);
 
   useEffect(() => {
-    // en este punto, el manager está vacío, pásaselo a la function del domain que va a
-    // fetch the tasks from localstorage and bulk load them to the task manager
-
     const subscription = wipman.tasks$.subscribe((taskMap) => {
       const unsortedTasks: Task[] = [...taskMap.values()];
       console.log(`TaskExplorer::wipman.tasks$ emitted latest task list`);
@@ -34,61 +27,8 @@ function TaskExplorer({ wipman }: TaskExplorerProps) {
     return subscription.unsubscribe;
   }, [wipman]);
 
-  useEffect(() => {
-    const subscription = wipman.status$.subscribe((status) => {
-      switch (status) {
-        case WipmanStatus.BrowserLoadStarted:
-          setShowSpinner(true);
-          break;
-        case WipmanStatus.BrowserLoadCompleted:
-          setShowSpinner(false);
-          break;
-        case WipmanStatus.BackendLoadStarted:
-          setShowSpinner(true);
-          break;
-        case WipmanStatus.BackendLoadCompleted:
-          setShowSpinner(false);
-          break;
-        case WipmanStatus.AddTaskInApiStarted:
-          setShowSpinner(true);
-          break;
-        case WipmanStatus.AddTaskInApiCompleted:
-          setShowSpinner(false);
-          break;
-        case WipmanStatus.UpdateTaskInApiStarted:
-          setShowSpinner(true);
-          break;
-        case WipmanStatus.UpdateTaskInApiCompleted:
-          setShowSpinner(false);
-          break;
-        case WipmanStatus.DeleteTaskInApiStarted:
-          setShowSpinner(true);
-          break;
-        case WipmanStatus.DeleteTaskInApiCompleted:
-          setShowSpinner(false);
-          break;
-        case WipmanStatus.InitStarted:
-          break;
-        case WipmanStatus.InitCompleted:
-          break;
-        case WipmanStatus.AddViewInStoreStarted:
-          break;
-        case WipmanStatus.AddViewInStoreCompleted:
-          break;
-        case WipmanStatus.RemoveViewFromStoreStarted:
-          break;
-        case WipmanStatus.RemoveViewFromStoreCompleted:
-          break;
-        default:
-          assertNever(status, `Unsupported WipmanStatus variant: ${status}`);
-      }
-    });
-
-    return subscription.unsubscribe;
-  }, [wipman]);
-
   function openTask(id: TaskId): void {
-    navigate(getTaskPath(id));
+    navigateTo(getTaskPath(id));
   }
 
   function addTask(title: TaskTitle): void {
