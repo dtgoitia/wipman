a295164fe26c45f61326db0f1a660384fbf25e26
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-05-10 08:37:09 +0100

    feat(api): add logs to CLI

--- api/src/use_cases/files_to_db.py
+++ api/src/use_cases/files_to_db.py
@@ -1,10 +1,16 @@
+import logging
+
 from src.adapter.fs import load_wipman_dir
 from src.adapter.sqlite import DbClient
 from src.config import Config
 
+logger = logging.getLogger(__name__)
+
 
 def dump_wipman_dir_to_db(config: Config) -> None:
+    logger.info(f"Reading wipman directory: {config.wipman_dir.absolute()}")
     views, tasks = load_wipman_dir(config=config)
 
     db = DbClient(config=config)
     db.dump_wipman(views=views, tasks=tasks)
+    logger.info(f"Data saved to: {config.db_path.absolute()}")
