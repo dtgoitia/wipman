9f6fe31d7facfac55c1107eb31c52d9dc669bfe3
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-05-12 06:15:24 +0100

    devex(ui): improve build messages

--- scripts/build_webapp_only_in_master.sh
+++ scripts/build_webapp_only_in_master.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Checking branch..."
+echo -n "Checking branch..."
 current_branch=$(git branch --show-current)
-echo "current_branch=$current_branch"
+echo " current branch: $current_branch"
 
 if [ "${current_branch}" != "master" ]; then
     echo "Aborting build as you are not in 'master' branch"
     exit 0
 fi
 
-echo "Cool, you are in 'master' branch"
+echo "Cool, you are in 'master' branch, let's build the webapp"
 
 scripts/build_webapp.sh
