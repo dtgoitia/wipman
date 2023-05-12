a496ba8f8580f7dcd5ab7329eef58e534cb8f41a
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-05-12 06:54:47 +0100

    devex(ui): only build webapp if webapp dir has changed

--- scripts/build_webapp_only_in_master.sh
+++ scripts/build_webapp_only_in_master.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
 
-echo -n "Checking branch..."
-current_branch=$(git branch --show-current)
-echo " current branch: $current_branch"
+set -eu
 
-if [ "${current_branch}" != "master" ]; then
-    echo "Aborting build as you are not in 'master' branch"
+target_branch="master"
+target_dir="webapp"
+
+echo -n "Ensuring current branch is '${target_branch}'... "
+current_branch=$(git branch --show-current)
+echo "done"
+
+if [ "${current_branch}" != "${target_branch}" ]; then
+    echo "Stopping build: must be in '${target_branch}' branch to build"
     exit 0
 fi
 
-echo "Cool, you are in 'master' branch, let's build the webapp"
+last_ref="origin/master"
+current_ref="HEAD"
+target_path="$(realpath "${target_dir}")"
+echo -n "Looking for code changes in '${target_path}' directory... "
+
+changed_paths="$( \
+    git diff --name-only "${last_ref}" "${current_ref}" \
+    | xargs realpath
+)"
+webapp_changes="$(echo -n "${changed_paths}" | grep "${target_path}/" || echo "no_changes_found")"
+echo "done"
+
+if [ "${webapp_changes}" == "no_changes_found" ]; then
+    echo "Stopping build: no changes found in '${target_dir}' directory"
+    exit 0
+fi
 
 scripts/build_webapp.sh
