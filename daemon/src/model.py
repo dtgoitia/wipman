2fc6d777c5a843847ee4805eddf3042352dfd299
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-06-02 09:19:17 +0100

    feat(daemon): add `Task` and `View` models

---
+++ daemon/src/model.py
@@ -0,0 +1,37 @@
+import datetime
+from dataclasses import dataclass
+from typing import TypeAlias
+
+Hash: TypeAlias = str
+ISODatetimeString: TypeAlias = str  # "2022-07-19T07:11:00+01:00"
+MarkdownString: TypeAlias = str
+Tag: TypeAlias = str
+TaskContent: TypeAlias = MarkdownString | None
+TaskId: TypeAlias = str
+TaskTitle: TypeAlias = str
+ViewContent: TypeAlias = str
+ViewId: TypeAlias = str
+ViewTitle: TypeAlias = str
+
+
+@dataclass(frozen=True)
+class Task:
+    id: TaskId
+    title: TaskTitle
+    created: datetime.datetime
+    updated: datetime.datetime
+    tags: frozenset[Tag]
+    blocked_by: frozenset[TaskId]
+    blocks: frozenset[TaskId]
+    completed: bool
+    content: TaskContent
+
+
+@dataclass(frozen=True)
+class View:
+    id: ViewId
+    title: ViewTitle
+    created: datetime.datetime
+    updated: datetime.datetime
+    tags: frozenset[Tag]
+    task_ids: list[TaskId]
