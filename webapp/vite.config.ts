f865239102ef0a6c33ed3f644db5512414363519
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-06-09 14:11:39 +0100

    devex(ui): use TypeScript to type-check on development server
    As opposed to waiting until build time to type-check.

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,11 +1,18 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 const port = 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+    }),
+  ],
   server: {
     port,
     host: true, // expose in LAN -- needed to run containerized
