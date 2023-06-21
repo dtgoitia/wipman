46ccfd936734ef8371bab94cc8749cd6fac4a69d
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-06-21 18:32:10 +0100

    refactor(ui): move `<ViewTitleComponent/>` to a separate file

--- webapp/src/pages/ViewPage/ViewDetail.tsx
+++ webapp/src/pages/ViewPage/ViewDetail.tsx
@@ -1,13 +1,12 @@
 import { DeleteConfirmationDialog } from "../../components/DeleteConfirmationDialog";
-import InputText from "../../components/InputText";
 import { TagSelector } from "../../components/TagSelector";
 import { nowIsoString } from "../../domain/dates";
 import { setsAreEqual } from "../../domain/set";
 import { Tag, View, ViewTitle } from "../../domain/types";
 import { Wipman } from "../../domain/wipman";
+import { ViewTitleComponent } from "./ViewTitle";
 import { Button } from "primereact/button";
 import { useState } from "react";
-import styled from "styled-components";
 
 interface ViewDetailProps {
   view: View;
@@ -67,73 +66,5 @@ export function ViewDetail({ view, wipman }: ViewDetailProps) {
         </div>
       )}
     </div>
-  );
-}
-
-const StyledViewTitle = styled.div`
-  margin: 1rem 0;
-  display: flex;
-  flex-flow: row nowrap;
-  gap: 0.3rem;
-`;
-
-const ViewTitleText = styled.h2`
-  display: inline;
-  margin: 0;
-  padding: 0;
-  font-size: 2rem;
-`;
-
-interface ViewTitleProps {
-  title: ViewTitle;
-  onUpdate: (title: ViewTitle) => void;
-}
-function ViewTitleComponent({
-  title: originalTitle,
-  onUpdate: updateTitle,
-}: ViewTitleProps) {
-  const [title, setTitleLocally] = useState<ViewTitle>(originalTitle);
-  const [isEditing, setIsEditing] = useState<boolean>(false);
-  const isUnsaved: boolean = originalTitle !== title;
-
-  if (isEditing === false) {
-    return (
-      <StyledViewTitle>
-        <ViewTitleText>{title}</ViewTitleText>
-        <Button
-          icon="pi pi-pencil"
-          className="p-button-rounded p-button-text p-button-sm"
-          onClick={() => setIsEditing(true)}
-        />
-      </StyledViewTitle>
-    );
-  }
-
-  const untitled: ViewTitle = "untitled";
-
-  function handleChange(value: string | undefined): void {
-    setTitleLocally(value === undefined ? untitled : value);
-  }
-
-  function handleSave(): void {
-    setIsEditing(false);
-    updateTitle(title === undefined ? untitled : title);
-  }
-
-  return (
-    <StyledViewTitle>
-      <InputText
-        id="task-title"
-        fill
-        placeholder="Task title"
-        value={title === untitled ? undefined : title}
-        onChange={handleChange}
-      />
-      <Button
-        icon={isUnsaved ? "pi pi-save" : "pi pi-times"}
-        className="p-button-rounded p-button-text p-button-sm"
-        onClick={handleSave}
-      />
-    </StyledViewTitle>
   );
 }
---
+++ webapp/src/pages/ViewPage/ViewTitle.tsx
@@ -0,0 +1,74 @@
+import InputText from "../../components/InputText";
+import { ViewTitle } from "../../domain/types";
+import { Button } from "primereact/button";
+import { useState } from "react";
+import styled from "styled-components";
+
+const StyledViewTitle = styled.div`
+  margin: 1rem 0;
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 0.3rem;
+`;
+
+const ViewTitleText = styled.h2`
+  display: inline;
+  margin: 0;
+  padding: 0;
+  font-size: 2rem;
+`;
+
+interface Props {
+  title: ViewTitle;
+  onUpdate: (title: ViewTitle) => void;
+}
+
+export function ViewTitleComponent({
+  title: originalTitle,
+  onUpdate: updateTitle,
+}: Props) {
+  const [title, setTitleLocally] = useState<ViewTitle>(originalTitle);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const isUnsaved: boolean = originalTitle !== title;
+
+  if (isEditing === false) {
+    return (
+      <StyledViewTitle>
+        <ViewTitleText>{title}</ViewTitleText>
+        <Button
+          icon="pi pi-pencil"
+          className="p-button-rounded p-button-text p-button-sm"
+          onClick={() => setIsEditing(true)}
+        />
+      </StyledViewTitle>
+    );
+  }
+
+  const untitled: ViewTitle = "untitled";
+
+  function handleChange(value: string | undefined): void {
+    setTitleLocally(value === undefined ? untitled : value);
+  }
+
+  function handleSave(): void {
+    setIsEditing(false);
+    updateTitle(title === undefined ? untitled : title);
+  }
+
+  return (
+    <StyledViewTitle>
+      <InputText
+        id="task-title"
+        fill
+        placeholder="Task title"
+        value={title === untitled ? undefined : title}
+        onChange={handleChange}
+      />
+      <Button
+        icon={isUnsaved ? "pi pi-save" : "pi pi-times"}
+        className="p-button-rounded p-button-text p-button-sm"
+        onClick={handleSave}
+      />
+    </StyledViewTitle>
+  );
+}
