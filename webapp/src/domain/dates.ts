587c5619fdd82cbf483071fa9e7d0bd1d8ca7cdf
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-06-24 08:16:50 +0100

    feat(ui): support ISO and locale date formatting

--- webapp/src/domain/dates.ts
+++ webapp/src/domain/dates.ts
@@ -8,6 +8,47 @@ export function nowIsoString(): ISODatetimeString {
   return now().toISOString();
 }
 
+export function parseDate(strDate: ISODatetimeString): Date {
+  return new Date(strDate);
+}
+
+// https://devhints.io/wip/intl-datetime
+const LANGUAGE_SIMILAR_TO_ISO8601 = "sv-SE";
+
+export const isoDateTimeFormatter = new Intl.DateTimeFormat(
+  LANGUAGE_SIMILAR_TO_ISO8601,
+  {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }
+).format;
+
+function getLocalTimezoneFormatted(): string {
+  const offset = new Date().getTimezoneOffset(); // in minutes
+
+  const sign = offset <= 0 ? "+" : "-";
+
+  const m = offset % 60;
+  const h = (offset - m) / 60;
+
+  const fmt = (n: number) => String(Math.abs(n)).padStart(2, "0");
+
+  return `${sign}${fmt(h)}:${fmt(m)}`;
+}
+
+/**
+ * Produces a date like `2023-05-22 16:08:27 +01:00`.
+ */
+export function dateToLocale(date: Date): string {
+  const naive: string = isoDateTimeFormatter(date);
+  const tz = getLocalTimezoneFormatted();
+  return `${naive} ${tz}`;
+}
+
 export class Timedelta {
   /**
    * Total amount of seconds in the delta.
