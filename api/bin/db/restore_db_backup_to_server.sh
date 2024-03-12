212b1385a266e81f8bb75a66b42f56df43723425
David Torralba Goitia <david.torralba.goitia@gmail.com>
2024-03-12 15:21:52 +0000

    devex(api): update script to restore local DB to server

--- api/bin/db/restore_db_backup_to_server.sh
+++ api/bin/db/restore_db_backup_to_server.sh
@@ -2,15 +2,15 @@
 
 # Usage example
 # cd projects/wipman/api
-# API_BASE_URL=88.198.150.140 bash api/bin/db/restore_db_backup_to_server.sh kk.db
+# K3S_CLUSTER_URL=88.198.150.140 bash api/bin/db/restore_db_backup_to_server.sh kk.db
 
 set -eu
 
 db_backup_file="$1"
 remote_user="epo"
 
-if [[ -z "${API_BASE_URL}" ]]; then
-    echo "Please set API_BASE_URL environment variable"
+if [[ -z "${K3S_CLUSTER_URL}" ]]; then
+    echo "Please set K3S_CLUSTER_URL environment variable"
     exit 1
 fi
 
@@ -18,7 +18,7 @@ db_backup_path="$(pwd)/${db_backup_file}"
 
 # Push file to server
 source="${db_backup_path}"
-destiny="${remote_user}@${API_BASE_URL}:/home/${remote_user}/projects/wipman/api/wipman.db"
+destiny="${remote_user}@${K3S_CLUSTER_URL}:/home/${remote_user}/k3s-data/wipman-api-db.sqlite"
 echo "Sending file over SSH:"
 echo "  from: ${source}"
 echo "  to:   ${destiny}"
